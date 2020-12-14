5c2d4fcfbe650415babca9df94710dfa4b9676fd
Jakub Szymczak <jakubszymczak@Jakubs-MacBook-Pro.local>
2020-12-14 22:58:02 +0000

    Exercise 02

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,27 +1,36 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings ;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  
+  return person.city == "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  var counter = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] == "sheep") counter++;
+  }
+  return counter;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-}
+  if  (person.address.postCode.substring(0, 1) == "M" && person.address.city === "Manchester") {
+      return true;
+    }
+  return false;
+  }
+  
 
 module.exports = {
   getFillings,
